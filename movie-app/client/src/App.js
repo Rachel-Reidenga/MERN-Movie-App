04e5818e363cf02fc5ce43c4552c150be93cc141
Anthonyz28 <anthony.z28@hotmail.com>
2021-05-23 09:40:19 -0500

    concurrently

---
+++ movie-app/client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchMovies from './pages/SearchMovies';
+import SavedMovies from './pages/SavedMovies';
+import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchMovies} />
+            <Route exact path='/saved' component={SavedMovies} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ movie-app/client/src/pages/SavedBooks.js
@@ -0,0 +1,86 @@
+// ***STILL NEED TO UPDATE
+
+import React from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+
+// import apollo queries
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_MOVIE } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+import { removeMovieId } from '../utils/localStorage';
+
+
+const SavedMovies = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeMovie, { error }] = useMutation(REMOVE_MOVIE);
+
+  const userData = data?.me || {};
+
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+
+
+  // create function that accepts the movie's mongo _id value as param and deletes the movie from the database
+  const handleDeleteMovie = async (movieId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await removeMovie({
+        variables: { movieId },
+      });
+
+      // upon success, remove movie's id from localStorage
+      removeMovieId(movieId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing saved movies!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedMoviess.length
+            ? `Viewing ${userData.savedMovies.length} saved ${userData.savedMovies.length === 1 ? 'movie' : 'movies'}:`
+            : 'You have no saved movies!'}
+        </h2>
+        <CardColumns>
+          {userData.savedMovies.map((movie) => {
+            return (
+              <Card key={movie.movieId} border='dark'>
+                {movie.image ? <Card.Img src={movie.image} alt={`The cover for ${movie.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  {/* should this not be 'authors'? */}
+                  <p className='small'>Authors: {movie.authors}</p> 
+                  <Card.Text>{movie.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteMovie(movie.movieId)}>
+                    Delete this Movie!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedMovies;
